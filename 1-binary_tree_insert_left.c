6bbd7418da6a24fb4f9df79cd3b190d2cf66b11e
Beardocracy <travisjbearden@gmail.com>
2020-03-09 22:55:52 +0000

    Changed it back to return null if parent is null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,8 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 	{
-		new = binary_tree_node(NULL, value);
-		return (new);
+		return (NULL);
 	}
 
 	if (parent->left == NULL)
