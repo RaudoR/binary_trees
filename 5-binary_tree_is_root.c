5cf02a2372da1653b1dab3e008c24f526856407c
Beardocracy <travisjbearden@gmail.com>
2020-03-09 22:40:13 +0000

    Added part 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,9 +8,8 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->n != NULL)
-	{
-		if (node->parent == NULL)
-			return (1);
-	}
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
 }
