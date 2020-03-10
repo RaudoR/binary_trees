33a38ac51907a83daea1936b335308c00cd070c6
Beardocracy <travisjbearden@gmail.com>
2020-03-10 20:12:51 +0000

    Added prototypes for 16

--- binary_trees.h
+++ binary_trees.h
@@ -42,7 +42,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
-int tree_is_perfect_help(const binary_tree_t *tree, size_t depth, size_t level);
+int tree_is_perfect_help(const binary_tree_t *tree, int depth, int level);
+int binary_tree_left_max_depth(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
