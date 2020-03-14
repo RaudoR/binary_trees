316c6cdd754200ac542f05cabeed406649c664fa
Bob Deprizio <robert.deprizio@gmail.com>
2020-03-14 03:23:40 +0000

    Added binary_tree_is_bst as a helper function instead of seperate file

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,5 +1,38 @@
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is a valid BST
+ * @tree: pointer to the root of the tree to be checked
+ * Return: 1 if valid, 0 if not.
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (bt_is_bst_helper((binary_tree_t *)tree, INT_MIN, INT_MAX));
+}
+
+/**
+ * bt_is_bst_helper - checks a binary tree is a valid BST
+ * @node: pointer to the tree to be checked
+ * @min: the min value a node can be
+ * @max: the max value a node can be
+ * Return: 1 if valid, 0 otherwise
+ */
+int bt_is_bst_helper(binary_tree_t *node, int min, int max)
+{
+	if (node == NULL)
+		return (1);
+
+	if (node->n < min || node->n > max)
+		return (0);
+
+	return (bt_is_bst_helper(node->left, min, node->n - 1) &&
+		bt_is_bst_helper(node->right, node->n + 1, max));
+}
+
+
 /**
  * max - helper function that returns max value of 2 ints
  * @x: int 1
@@ -24,6 +57,7 @@ int height(binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+
 	return (1 + max(height(tree->left), height(tree->right)));
 }
 
