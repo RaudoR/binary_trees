cc7c76c72b4971638e7b80e55b25f2830d83bd57
Bob Deprizio <robert.deprizio@gmail.com>
2020-04-10 18:10:10 +0000

    Adding sorted_array_to_avl func

---
+++ 124-sorted_array_to_avl.c
@@ -0,0 +1,72 @@
+#include "binary_trees.h"
+
+/**
+ * sorted_array_to_avl2 - main function that adds nodes to create an AVL tree
+ * @arr:  pointer to the array to be converted
+ * @start: beginning index of size_t for current recursion
+ * @end: ending index of size_t for current recursion
+ * Return: pointer to the root node of the created AVL tree
+ */
+
+avl_t *sorted_array_to_avl2(int *arr, size_t start, size_t end)
+{
+	size_t mid;
+	avl_t *root, *temp;
+
+	if (start - 1 == end)
+		return (NULL);
+
+	mid = start + (end - start) / 2;
+
+	temp = malloc(sizeof(avl_t));
+	temp->parent = temp;
+
+	root = binary_tree_node(temp, arr[mid]);
+
+	free(temp);
+
+	root->left = sorted_array_to_avl2(arr, start, mid - 1);
+
+	if (root->left)
+		root->left->parent = root;
+
+	root->right = sorted_array_to_avl2(arr, mid + 1, end);
+
+	if (root->right)
+		root->right->parent = root;
+
+	return (root);
+}
+
+
+/**
+ * sorted_array_to_avl - function that builds an AVL tree from an array
+ * @array: pointer to the first element of the array to be converted
+ * @size:number of elements in the array
+ * Return: pointer to the root node of the created AVL tree
+ */
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	avl_t *node;
+
+	if (size == 0)
+		return (NULL);
+
+	if (size == 1)
+	{
+		node = binary_tree_node(NULL, array[0]);
+		return (node);
+	}
+
+	if (size == 2)
+	{
+		node = binary_tree_node(NULL, array[0]);
+		node->right = binary_tree_node(node, array[1]);
+		return (node);
+	}
+
+	node = sorted_array_to_avl2(array, 0, size - 1);
+
+	return (node);
+}
