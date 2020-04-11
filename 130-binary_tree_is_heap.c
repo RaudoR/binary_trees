80c8539c431fca452dd6aba05f3d3e9c06754cdc
Bob Deprizio <robert.deprizio@gmail.com>
2020-04-11 00:31:13 +0000

    Added edge case

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -80,6 +80,9 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 	unsigned int node_count = countNodes(tree);
 	unsigned int index = 0;
 
+	if (tree == NULL)
+		return (0);
+
 	if (isCompleteUtil(tree, index, node_count) && isHeapUtil(tree))
 		return (1);
 	return (0);
